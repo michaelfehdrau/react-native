6d908a2d0531f475286bee5681077dd15960a804
Michael Fehdrau <mfehdrau@hotmail.com>
2023-05-20 13:48:37 -0400

    Social Sharing

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,9 +1,8 @@
-import { StyleSheet, Text, View, PanResponder, Alert } from 'react-native';
+import { StyleSheet, Text, View, PanResponder, Alert, Share } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { baseUrl } from '../../shared/baseUrl';
 import * as Animatable from 'react-native-animatable';
 import { useRef } from 'react';
-
 
 const RenderCampsite = (props) => {
     const { campsite } = props;
@@ -49,6 +48,19 @@ const RenderCampsite = (props) => {
         },
     });
 
+    const shareCampsite = (title, message, url) => {
+        Share.share(
+            {
+                title,
+                message: `${title}: ${message} ${url}`,
+                url
+            },
+            {
+                dialogTitle: 'Share ' + title
+            }
+        )
+    };
+
     if (campsite) {
         return (
             <Animatable.View
@@ -87,6 +99,20 @@ const RenderCampsite = (props) => {
                             raised
                             reverse
                             onPress={() => props.onShowModal()}
+                        />
+                        <Icon
+                            name='share'
+                            type='font-awesome'
+                            color='#5637DD'
+                            raised
+                            reverse
+                            onPress={() =>
+                                shareCampsite(
+                                    campsite.name,
+                                    campsite.description,
+                                    baseUrl + campsite.image
+                                )
+                            }
                         />
                     </View>
                 </Card>
